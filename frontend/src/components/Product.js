9cc227f91855549c0581e11efab0360b316a683c
Yan <jan.kozhyn@gmail.com>
2023-04-24 16:40:33 +0300

    Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
+import Rating from './Rating'
+
 function Product({ product }) {
   return (
     <Card className="my-3 p-3 rounded">
@@ -17,13 +19,16 @@ function Product({ product }) {
 
         <Card.Text as="div">
           <div className="my-3">
-            {product.rating} from {product.numReviews} reviews          
+            
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color={'#f8e825'}
+            />
           </div>
         </Card.Text>
 
-        <Card.Text as="h3">
-            ${product.price}
-        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
