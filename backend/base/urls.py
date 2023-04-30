98b12742b87b9b7e49e4b0aa9cf08077cc42954a
Yan <jan.kozhyn@gmail.com>
2023-04-30 04:22:10 +0300

    JSON Web Token Installion

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 from . import views 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('product/<str:pk>', views.getProduct, name="product"),
+    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
